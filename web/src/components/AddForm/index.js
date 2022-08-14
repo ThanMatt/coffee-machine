a127777299d94f0117923d85ccb514b8d1652f7a
Thanmatt <aethanilagan@gmail.com>
2022-08-14 12:33:49 +0800

    Added yup

--- web/src/components/AddForm/index.js
+++ web/src/components/AddForm/index.js
@@ -9,10 +9,19 @@ import {
   Textarea
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useLocation } from 'react-router-dom';
 import axios from '../../config/axios';
 
 const AddForm = () => {
+  const location = useLocation();
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useForm();
+
   const [categories, setCategories] = useState([]);
   useEffect(() => {
     const getCategories = async () => {
@@ -23,27 +32,36 @@ const AddForm = () => {
     getCategories();
   }, []);
   return (
-    <Flex>
-      <FormControl>
-        <FormLabel>Product Name</FormLabel>
-        <Input type="email" />
-        <FormLabel mt="16px">Product Category</FormLabel>
-        <Select placeholder="Select a product category">
-          {categories.map((category) => {
-            return (
-              <option key={category._id} value={category._id}>
-                {category.name}
-              </option>
-            );
-          })}
-        </Select>
-        <FormLabel mt="16px">Product Description</FormLabel>
-        <Textarea />
+    <form>
+      <Flex flexDirection="column">
+        <FormControl isInvalid={errors.name}>
+          <FormLabel>Product Name</FormLabel>
+          <Input type="email" />
+        </FormControl>
+
+        <FormControl mt="16px" isInvalid={errors.category}>
+          <FormLabel>Product Category</FormLabel>
+          <Select placeholder="Select a product category">
+            {categories.map((category) => {
+              return (
+                <option key={category._id} value={category._id}>
+                  {category.name}
+                </option>
+              );
+            })}
+          </Select>
+        </FormControl>
+
+        <FormControl mt="16px" isInvalid={errors.description}>
+          <FormLabel>Product Description</FormLabel>
+          <Textarea />
+        </FormControl>
+
         <Flex mt="16px" justifyContent="flex-end">
           <Button>Submit</Button>
         </Flex>
-      </FormControl>
-    </Flex>
+      </Flex>
+    </form>
   );
 };
 
